6828eb649e841ae6aba7b228f452bafabf0c4941
Raphael Ribeiro <raphar.moura@gmail.com>
2024-09-27 08:38:35 -0300

    Correcao

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,177 +1,120 @@
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-footer {
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  align-items: center;
-  background-color: black;
-}
-
-/* Estilos gerais */
-.caixinhas {
-  color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-  text-align: center;
-}
-
-.linkfooter {
-  text-decoration: none;
-  color: #c94e1a;
-}
-
-.linkfooter:hover {
-  text-decoration: underline;
-}
-
-.filhosFooter {
-  display: flex;
-  flex-direction: row;
-  margin: 2rem;
-  justify-content: space-around;
-  flex-wrap: wrap;
-}
-
-.filhosFooter div {
-  margin: 1rem;
-}
-
-.redesSociais {
-  color: #c94e1a;
-  padding: 0.8rem 0.7rem;
-  border-radius: 20%;
-  background-color: rgba(41, 41, 41, 0.685);
-}
-
-.siga-nos {
-  color: white;
-  margin: 1rem 0;
-}
-
-.cadastrar {
-  border: 1px solid #c94e1a;
-  box-shadow: 1px 2px #c94e1a;
-  border-radius: 15%;
-  padding: 0.8rem;
-  background-color: whitesmoke;
-}
-
-.cadastrarSe {
-  color: black;
-  font-weight: bolder;
-}
-
-.info {
-  color: white;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: flex-end;
-  width: 99%;
-}
-/* Media Queries */
-
-/* 4K - Tamanho maior */
-@media screen and (min-width: 2560px) {
-  .filhosFooter {
-    margin: 4rem;
-    gap: 2rem;
-  }
-
-  .redesSociais {
-    padding: 0.8rem 1rem;
-    font-size: 2rem;
-  }
-
-  .caixinhas {
-    font-size: 1.5rem;
-  }
-
-  .cadastrar {
-    padding: 1.5rem;
-  }
-}
-
-/* 1440px */
-@media screen and (min-width: 1440px) and (max-width: 2559px) {
-  .filhosFooter {
-    margin: 3rem;
-  }
-
-  .redesSociais {
-    font-size: 1.5rem;
-    padding: 0.8rem 0.7rem;
-  }
-
-  .caixinhas {
-    font-size: 1.2rem;
-  }
-
-  .cadastrar {
-    padding: 1rem;
-  }
-}
-
-/* Tablet - 760px */
-@media screen and (max-width: 760px) {
-  .filhosFooter {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-row: 1fr 1fr;
-    align-items: center;
-  }
-  .siga-nos {
-    grid-column-start: 1;
-    grid-column-end: 3;
-  }
-  .redesSociais {
-    font-size: 1rem;
-    padding: 0.8rem 0.7rem;
-  }
-
-  .caixinhas {
-    font-size: 1rem;
-  }
-
-  .cadastrar {
-    grid-column-start: 1;
-    grid-column-end: 3;
-    padding: 0.8rem;
-  }
-}
-
-/* Mobile Large - 425px */
-@media screen and (max-width: 425px) {
-  .filhosFooter {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-row: 1fr 1fr;
-    align-items: center;
-  }
-  .siga-nos {
-    grid-column-start: 1;
-    grid-column-end: 3;
-  }
-  .redesSociais {
-    font-size: 1rem;
-    padding: 0.4rem 0.5rem;
-  }
-
-  .caixinhas {
-    font-size: 0.9rem;
-  }
-
-  .cadastrar {
-    grid-column-start: 1;
-    grid-column-end: 3;
-    padding: 0.7rem;
-  }
+import "./styles.css";
+import { ReactSocialMediaIcons } from "react-social-media-icons";
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <div class="filhosFooter Redes">
+          <div class="cadastrar">
+            <a href="" class="linkfooter cadastrarSe">
+              CADASTRE-SE
+            </a>
+          </div>
+          <h3 class="siga-nos">Siga-nos</h3>
+          <div class="redesSociais">
+            <ReactSocialMediaIcons
+              borderColor="rgba(0,0,0,0.25)"
+              icon="facebook"
+              iconColor="#c94e1a"
+              backgroundColor="black"
+            />
+          </div>
+          <div class="redesSociais">
+            <ReactSocialMediaIcons
+              borderColor="rgba(0,0,0,0.25)"
+              icon="instagram"
+              backgroundColor="black"
+              iconColor="#c94e1a"
+            />
+          </div>
+          <div class="redesSociais">
+            <ReactSocialMediaIcons
+              borderColor="rgba(0,0,0,0.25)"
+              icon="youtube"
+              backgroundColor="black"
+              iconColor="#c94e1a"
+            />
+          </div>
+          <div class="redesSociais">
+            <ReactSocialMediaIcons
+              borderColor="rgba(0,0,0,0.25)"
+              icon="linkedin"
+              backgroundColor="black"
+              iconColor="#c94e1a"
+            />
+          </div>
+          <div class="redesSociais">
+            <ReactSocialMediaIcons
+              borderColor="rgba(0,0,0,0.25)"
+              icon="twitter"
+              backgroundColor="black"
+              iconColor="#c94e1a"
+            />
+          </div>
+        </div>
+        <div class="filhosFooter">
+          <div class="caixinhas">
+            <h3>TOP FILMES DA SEMANA</h3>
+            <p>
+              <a href="" target="_blank" class="linkfooter">
+                Filme-1
+              </a>
+            </p>
+            <p>
+              <a href="" target="_blank" class="linkfooter">
+                Filme-2
+              </a>
+            </p>
+            <p>
+              <a href="" target="_blank" class="linkfooter">
+                Filme-3
+              </a>
+            </p>
+            <p>
+              <a href="" target="_blank" class="linkfooter">
+                Filme-4
+              </a>
+            </p>
+            <p>
+              <a href="" target="_blank" class="linkfooter">
+                Filme-5
+              </a>
+            </p>
+          </div>
+          <div class="caixinhas">
+            <h3>SNACK BAR</h3>
+            <p>
+              <a href="" target="_blank" class="linkfooter">
+                Snack Bar
+              </a>
+            </p>
+          </div>
+          <div class="caixinhas">
+            <h3>PARA EMPRESAS</h3>
+            <p>
+              <a href="" target="_blank" class="linkfooter">
+                Reserva de salas
+              </a>
+            </p>
+            <p>
+              <a href="" target="_blank" class="linkfooter">
+                Vouchers
+              </a>
+            </p>
+          </div>
+          <div class="caixinhas">
+            <h3>CONTATO</h3>
+            <p>
+              <a target="_blank" href="" class="linkfooter">
+                Central de atendimento
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="info">
+          <p>Insira aqui o nome da empresa &copy;</p>
+        </div>
+      </footer>
+    </>
+  );
 }
