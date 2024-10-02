2d58bf9cc565a173995243cfd4d9bc3ce390183c
PedroCeglia <dev.ceglia.pedro@gmail.com>
2024-10-02 14:53:52 -0300

    Import "FilmesList" component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Card from './components/Card'
+import FilmesList from './components/FilmesList'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -8,10 +8,7 @@ export default function App(){
     return(
         <div className="App">
             <Header />
-            <div className="containerCards">
-                <Card />
-                <Card />
-            </div>
+            <FilmesList listaFilmes={[0,1,2,3,4,5,6,7,8,9,10]} title="Em Breve"/>
             <Footer/>
         </div>
     )
