b870b0861d58c7423f056427e95245a030f95bb5
PedroCeglia <dev.ceglia.pedro@gmail.com>
2024-10-09 15:12:39 -0300

    clean code

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,5 @@
 import FilmesList from "../../components/FilmesList";
 
 export default function Home(){
-    return <div>
-        <FilmesList listaFilmes={[0,1,2,3,4,5,6,7,8,9]} title="Em Breve"/>
-    </div>
+    return <FilmesList listaFilmes={[0,1,2,3,4,5,6,7,8,9]} title="Em Breve"/>
 }
