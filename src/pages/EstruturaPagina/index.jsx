55247d2b5ae76c8d0cf3af9cf133b9c19b98e071
PedroCeglia <dev.ceglia.pedro@gmail.com>
2024-10-09 14:39:39 -0300

    Add a style to EstruturaPagina

--- src/pages/EstruturaPagina/index.jsx
+++ src/pages/EstruturaPagina/index.jsx
@@ -1,10 +1,11 @@
+import styles from "./EstruturaPagina.module.css"
 import { Outlet } from "react-router-dom"
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
 
 export default function EstruturaPagina(){
     return (
-        <div>
+        <div className={styles.Container}>
             <Header/>
             <Outlet/>
             <Footer/>
