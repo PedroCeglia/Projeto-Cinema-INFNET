3f6dcabf6ee4c108b17aa7e380951c9d5589e1a3
PedroCeglia <dev.ceglia.pedro@gmail.com>
2024-12-19 07:01:43 -0300

    Rename "ChoiceSession" component to "ChooseSession"

--- src/components/choose/ChooseSession/index.jsx
+++ src/components/choose/ChooseSession/index.jsx
@@ -1,11 +1,11 @@
 import styles from "./styles.module.css"
 import { useEffect, useState } from "react"
 
-import SessionDayCard from "../SessionDayCard"
-import SessionRoomList from "../SessionRoomList"
+import SessionDayCard from "../../cards/SessionDayCard"
+import SessionRoomList from "../../listas/SessionRoomList"
 
-import { getDiasList } from "../../utils/Data"
-import ChoseSessionProvider from "../../contexts/ChoseSession"
+import { getDiasList } from "../../../utils/Data"
+import ChoseSessionProvider from "../../../contexts/ChoseSession"
 
 export default function ChoiceSession(){
     const [diasList, setDiasLista] = useState([])
@@ -21,12 +21,12 @@ export default function ChoiceSession(){
                 <div className={styles.ContainerDias}>
                     {
                         diasList.map((item, key) => {
-                            const {diaMes, diaSemana, mes, ano} = item
+                            const {diaMes, diaSemana, mesText, ano} = item
                             return <SessionDayCard 
                                 key={key}
                                 diaMes={diaMes}
                                 diaSemana={diaSemana}
-                                mes={mes}
+                                mes={mesText}
                                 ano={ano}
                             />
                         })
