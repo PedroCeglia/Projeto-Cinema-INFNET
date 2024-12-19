be816b82d061ff1868502731becaabc3aed6a4bd
PedroCeglia <dev.ceglia.pedro@gmail.com>
2024-12-19 07:00:58 -0300

    create SessionDayCard Component

---
+++ src/components/cards/SessionDayCard/index.jsx
@@ -0,0 +1,16 @@
+import styles from "./styles.module.css"
+import { useChoseSessionContext } from "../../../contexts/ChoseSession"
+
+export default function SessionDayCard({diaSemana, diaMes, mes, ano}){
+    const {setSelectedDate} = useChoseSessionContext()
+    const choseTheDate = () => {
+        setSelectedDate({diaSemana, diaMes, mes, ano})
+    }
+    return (
+            <article>
+                <button onClick={choseTheDate} className={styles.Container}>
+                    <p>{diaSemana}, {diaMes} {mes}</p>                
+                </button>
+            </article> 
+        )
+}
